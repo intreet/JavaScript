2e20a6d13504aefbcc4d5c128bbf9aebf499a967
intreet <117534821+intreet@users.noreply.github.com>
2023-02-15 22:57:13 +0300

    Create homeWork_wiles.js

---
+++ homeWork_wiles.js
@@ -0,0 +1,162 @@
+// "Use strict"
+// задание 1
+// let styles = ["Джаз", "Блюз"]
+// styles.push("Рок-н-рол")
+// let leng = styles.length
+// let mLeng = Math.round(leng / 2 - 1)// Находит середину массива
+// styles[mLeng] = "Класика"// Помещает значение в середину массива
+// console.log("Первый элемент массива:", styles[0])
+// styles.shift()
+// styles.unshift("Реп", "Регги")
+// console.log(styles) 
+
+// задание 2.1
+
+// let i = 1
+// while(i < 100){
+//     i++
+//     console.log(i)
+// }
+
+
+// //задание 2.2
+
+// let a = 100
+// while(a > 1){
+//     a--
+//     console.log(a)
+// }
+
+
+// задание 2.3
+
+// let g = 1
+// while (g < 100) {
+//     if(g % 2 == 0) {
+//         console.log(g)
+//     }
+//     else{}
+//     g++
+// }
+
+
+// задание 2.4
+
+// let mass = []
+// while (mass.length < 10) {
+//     mass.push("x")
+// }
+// console.log(mass)
+
+
+// задание 2.5
+
+// let f = 1
+// let mass = []
+// while(f <= 10){
+//     mass.push(f)
+//     f++
+// }
+// console.log(mass)
+
+
+
+// задание 2.6
+
+
+// let mass = []
+// while(mass.length < 10) {
+//     let randomNumber = ((Math.random() * 0.98 ) + 0.01).toFixed(2)
+//     mass.push(randomNumber)
+// }
+// console.log(mass)
+
+// задание 2.7
+
+
+// let mass = []
+// while(mass.length < 10) {
+// let randomNumber = Math.floor(Math.random() * 10) + 1
+//     mass.push(randomNumber)
+// }
+// console.log(mass)
+
+
+// задание 2.8
+
+// let mass = ["12", "34", "785", "3", "11", "2", "9", "0", "6", "5"]
+// b = 1 
+// while(b < 10){
+//     b++
+//     let object = mass[b]
+//     if(mass[object] > 0 && mass[object] < 10 ){
+//         console.log(object)
+//     }
+// }
+
+
+// задание 2.9
+
+// let mass = ["5", "34", "785", "3", "11", "2", "9", "0", "6", "9"]
+// let i = 0
+
+// while(i < mass.length){
+//     let object = mass[i]
+//     if(object == 5){
+//         console.log(object)
+//         break    
+//     }
+// } i++
+
+
+// задание 2.10
+
+// let mass = [5, 3, 8, 3, 12, 2, 9]
+// let summ = 0
+// let i = 0
+
+// while(i < mass.length){
+//     summ += mass[i]
+//     i++
+// }
+// console.log(summ)
+
+
+// задание 2.11
+
+// let mass = [5, 3, 8, 3, 12, 2, 9]
+// let summ = 0
+// let i = 0
+
+// while(i < mass.length){
+//     summ += mass[i] ** 2
+//     i++
+    
+// }
+// console.log(summ)
+
+
+// задание 2.12
+
+// let mass = [5, 3, 8, 3, 12, 2, 9]
+// let summ = 0
+// let i = 0
+
+// while(i < mass.length){
+//     summ += mass[i] 
+//     i++
+    
+// }
+
+// let sred = summ / mass.length
+// console.log(sred)
+
+
+
+
+
+
+
+
+
+
