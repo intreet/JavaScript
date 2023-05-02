b64ea35293b53a771c042bfd246c79e1687bf204
intreet <117534821+intreet@users.noreply.github.com>
2023-05-02 11:24:03 +0300

    Add files via upload

---
+++ практические работы/масивы_практическая работа.js
@@ -0,0 +1,97 @@
+// задание 1
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.splice(3, 0, "a", "b", "c");
+// console.log(arr);
+
+// задание 2
+
+// let arrey = {
+//   js: "test",
+//   jq: "hello",
+//   css: "world",
+// };
+
+// for (let name in arrey) {
+//   console.log(name);
+// }
+
+// задание 3";
+
+// let str = "http://2341gerfg.com";
+
+// if (str.includes("http://")) {
+//   console.log("строка содержит http://");
+// } else {
+//   console.log("строка не содержит http://");
+// }
+
+// задание 4
+
+// let a = "index.html";
+// if (a.includes(".html")) {
+//   console.log("строка содержит .html");
+// } else {
+//   console.log("строка не содержит .html");
+// }
+
+// задание 5
+
+// let str = ["1", "2", "3", "4", "5", "6"];
+// str.reverse();
+// console.log(str);
+
+// задание 6
+
+// let str = "var_test_text";
+
+// if (str.includes("_")) {
+//   let str2 = str.split("_");
+
+//   let a = str2[0];
+//   let b = str2[1];
+//   let c = str2[2];
+
+//   let b1 = b[0].toUpperCase();
+//   let b2 = b1 + b.toLowerCase().slice(1);
+
+//   let c1 = c[0].toUpperCase();
+//   let c2 = c1 + c.toLowerCase();
+
+//   let name = a + b2 + c2;
+//   console.log(name);
+// }
+
+// задание 7
+
+// let mass = ["я", "учу", "javascript", "!"];
+
+// mass.splice(0, 3, "Я", "УЧУ", "JAVASCRIPT");
+// console.log(mass);
+
+// задание 8
+
+// let date = ["2025", "-", "12", "-", "31"];
+
+// date.splice(1, 1, "/");
+// date.splice(3, 1, "/");
+// console.log(date);
+// date.reverse();
+// console.log(date);
+
+// задание 9
+
+// задание 10
+
+// let arr1 = ["1", "5", "2"];
+// let arr2 = ["1", "5", "2"];
+
+// for (let i = 0; i < arr1.length; i++) {
+//   if (arr1.toString() == arr2.toString()) {
+//     console.log("true");
+//     return true;
+//   } else {
+//     console.log("false");
+//     return false;
+//   }
+// }
